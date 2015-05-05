c61a0e806352649829b25193271c4e9b80224b7b
Renkaze <lionel.contoz@gmail.com>
2015-05-05 13:21:00 +0200

    Implémentation des classes

--- javaProjetMememoire/src/main/Rapport.java
+++ javaProjetMememoire/src/main/Rapport.java
@@ -4,11 +4,31 @@
  * and open the template in the editor.
  */
 package main;
+import java.util.*;
+
 
 /**
  *
  * @author Lionel CONTOZ
  */
 public class Rapport {
+  
+    protected String nom;
+    protected Date date;
     
+    public Rapport(String nom, Date date){
+        this.nom = nom;
+        this.date = date;
+    }
+    
+    public String getNom(){ return nom; }
+    public void setNom(String nom){ this.nom = nom; }
+    public Date getDate(){ return date; }
+    public void setDate(Date date){ this.date = date; }
+    
+    @Override
+    public String toString(){
+        return "Nom: " + nom + "\n" +
+                "Date: " + date + "\n";
+    }
 }
